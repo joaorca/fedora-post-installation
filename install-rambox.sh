7c7c1ce5411a56846c903094593bdba0d6fa4f11
JOAO R C ALMEIDA <joaorca@gmail.com>
2022-08-24 18:20:11 -0300

    Alteração de versões

--- install-rambox.sh
+++ install-rambox.sh
@@ -4,9 +4,9 @@ ERROR_COLOR='\033[7;31m'
 TITLE_COLOR='\033[1;31m'
 NC='\033[0m'
 
-RAMBOX_VERSION="0.8.0"
-RAMBOX_DISTRO="linux-x86_64"
-RAMBOX_URL="https://github.com/ramboxapp/community-edition/releases/download/${RAMBOX_VERSION}/Rambox-${RAMBOX_VERSION}-${RAMBOX_DISTRO}.rpm"
+RAMBOX_VERSION="2.0.6"
+RAMBOX_DISTRO="linux-x64"
+RAMBOX_URL="https://github.com/ramboxapp/download/releases/download/v${RAMBOX_VERSION}/Rambox-${RAMBOX_VERSION}-${RAMBOX_DISTRO}.rpm"
 RAMBOX_TMP_FILE="rambox-${RAMBOX_VERSION}-${RAMBOX_DISTRO}.rpm"
 
 if [ $(id -u) != 0 ]; then
@@ -21,7 +21,8 @@ fi
 echo -e "\n${TITLE_COLOR}Instalando RAMBOX${NC}"
 
 if ! [ -f /tmp/${RAMBOX_TMP_FILE} ]; then
-  wget -q ${RAMBOX_URL} --show-progress -O /tmp/${RAMBOX_TMP_FILE} 
+   echo -e ${RAMBOX_URL}
+   wget -q ${RAMBOX_URL} --show-progress -O /tmp/${RAMBOX_TMP_FILE} 
 fi
 dnf install /tmp/${RAMBOX_TMP_FILE} --assumeyes
 
